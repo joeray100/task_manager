5840d0608ffd6f98807d870c7b28ba90d09eaf50
Joe Ray <jray100.jr@gmail.com>
2021-05-07 00:02:30 -0600

    Finish C.R.U.D. functionality, project complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,6 @@ class TasksController < ApplicationController
   end
 
   def show
-    require 'pry'; binding.pry
     @task = Task.find(params[:id])
   end
 
@@ -12,14 +11,30 @@ class TasksController < ApplicationController
   end
 
   def create
-
     task = Task.new({
       title: params[:task][:title],
       description: params[:task][:description]
       })
-
     task.save
+    redirect_to '/tasks'
+  end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
     redirect_to '/tasks'
   end
 end
